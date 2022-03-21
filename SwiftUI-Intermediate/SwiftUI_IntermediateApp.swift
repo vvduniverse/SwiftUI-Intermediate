7faf667679171c6f6982c6bfd4d60c09be1c392e
Vahtee Boo <Vahtee.Boo@gmail.com>
2022-03-21 15:52:09 +0200

    Initial Commit

---
+++ SwiftUI-Intermediate/SwiftUI_IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntermediateApp.swift
+//  SwiftUI-Intermediate
+//
+//  Created by Vahtee Boo on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
