4503adb5bbac69bda4abaaa6a56132fb6cc7b5aa
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-27 17:32:34 +0300

    part-14-notation

---
+++ SwiftUI-Intermediate/CoreDataFetchRequestBC.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataFetchRequestBC.swift
+//  SwiftUI-Intermediate
+//
+//  Created by Vahtee Boo on 27.03.2022.
+//
+
+import SwiftUI
+
+struct CoreDataFetchRequestBC: View {
+    var body: some View {
+        Text("Look for information in a separate project!!!")
+    }
+}
+
+struct CoreDataFetchRequestBC_Previews: PreviewProvider {
+    static var previews: some View {
+        CoreDataFetchRequestBC()
+    }
+}
