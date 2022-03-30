e82bbc17bf52d5498977309cea17c4e3037851be
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-30 19:40:26 +0300

    part19

---
+++ SwiftUI-Intermediate/TypealiasBC.swift
@@ -0,0 +1,37 @@
+//
+//  Typealias.swift
+//  SwiftUI-Intermediate
+//
+//  Created by Vahtee Boo on 30.03.2022.
+//
+
+import SwiftUI
+
+struct MovierModel {
+    let title: String
+    let director: String
+    let count: Int
+}
+
+typealias TVModel = MovierModel
+
+struct TypealiasBC: View {
+    
+//    @State private var item = MovierModel(title: "Ukraine", director: "God", count: 10)
+    @State private var item = TVModel(title: "USA", director: "Baiden", count: 7)
+    
+    var body: some View {
+        VStack {
+            Text(item.title)
+            Text(item.director)
+            Text("\(item.count)")
+            Divider()
+        }
+    }
+}
+
+struct TypealiasBC_Previews: PreviewProvider {
+    static var previews: some View {
+        TypealiasBC()
+    }
+}
