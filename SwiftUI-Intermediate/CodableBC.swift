42337f6e2dd0da66f3ab2a7abfe601b753b2b07a
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-01 12:17:10 +0300

    minor changes

--- SwiftUI-Intermediate/CodableBC.swift
+++ SwiftUI-Intermediate/CodableBC.swift
@@ -13,35 +13,35 @@ struct CustomModel: Identifiable, Codable {
     let points: Int
     let isPremium: Bool
     
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case points
-        case isPremium
-    }
-    
-    init(id: String, name: String, points: Int, isPremium: Bool) {
-        self.id = id
-        self.name = name
-        self.points = points
-        self.isPremium = isPremium
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decode(String.self, forKey: .id)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.points = try container.decode(Int.self, forKey: .points)
-        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(points, forKey: .points)
-        try container.encode(isPremium, forKey: .isPremium)
-    }
+//    enum CodingKeys: String, CodingKey {
+//        case id
+//        case name
+//        case points
+//        case isPremium
+//    }
+//    
+//    init(id: String, name: String, points: Int, isPremium: Bool) {
+//        self.id = id
+//        self.name = name
+//        self.points = points
+//        self.isPremium = isPremium
+//    }
+//    
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.id = try container.decode(String.self, forKey: .id)
+//        self.name = try container.decode(String.self, forKey: .name)
+//        self.points = try container.decode(Int.self, forKey: .points)
+//        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
+//    }
+//    
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(id, forKey: .id)
+//        try container.encode(name, forKey: .name)
+//        try container.encode(points, forKey: .points)
+//        try container.encode(isPremium, forKey: .isPremium)
+//    }
 }
 
 class CodableViewModel: ObservableObject {
